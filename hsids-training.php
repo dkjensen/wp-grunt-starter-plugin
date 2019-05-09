ca4513dd38a6d9ea613571d58988f243d73bf84c
David Jensen <david@dkjensen.com>
2019-05-09 10:55:45 -0700

    Package replacements

--- hsids-training.php
+++ hsids-training.php
@@ -1,13 +1,13 @@
 <?php
 /**
- * Plugin Name: Easy Digital Downloads - Commissions Payouts
- * Description: Easy Digital Downloads Commissions Payouts API integration for FES and Commissions
+ * Plugin Name: WP Grunt Starter Plugin
+ * Description: 
  * Version: 1.0.0
  * Author: David Jensen
  * Author URI: https://dkjensen.com
- * Text Domain: hsids-training
+ * Text Domain: wp-grunt-starter-plugin
  *
- * @package HSIDS Training
+ * @package WP Grunt Starter Plugin
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -16,76 +16,18 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 global $wp_version;
 
-define( 'EDD_COMMISSIONS_PAYOUTS_VER', '1.0.0' );
-define( 'EDD_COMMISSIONS_PAYOUTS_PLUGIN_NAME', 'Easy Digital Downloads - Commissions Payouts' );
-define( 'EDD_COMMISSIONS_PAYOUTS_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
-define( 'EDD_COMMISSIONS_PAYOUTS_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
+define( 'WP_GRUNT_STARTER_PLUGIN_VER', '1.0.0' );
+define( 'WP_GRUNT_STARTER_PLUGIN_PLUGIN_NAME', 'WP Grunt Starter Plugin' );
+define( 'WP_GRUNT_STARTER_PLUGIN_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+define( 'WP_GRUNT_STARTER_PLUGIN_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 
-
-if ( ! file_exists( EDD_COMMISSIONS_PAYOUTS_PLUGIN_DIR . 'vendor/autoload.php' ) ) {
-    /**
-     * Display admin notice if Composer packages not installed
-     * 
-     * @codeCoverageIgnore
-     */
-    add_action( 'admin_notices', function() {
-        ?>
-
-        <div class="notice notice-error is-dismissible">
-            <p><?php printf( __( 'Error loading composer packages for %s', 'hsids-training' ), EDD_COMMISSIONS_PAYOUTS_PLUGIN_NAME ); ?></p>
-        </div>
-
-        <?php
-    } );
-
-    return;
-}
-
-if ( ! class_exists( 'Easy_Digital_Downloads' ) || version_compare( EDD_VERSION, '2.4', '<' ) ) {
-    /**
-     * Display admin notice if not up to date with EDD version
-     * 
-     * @codeCoverageIgnore
-     */
-    add_action( 'admin_notices', function() {
-        ?>
-        
-        <div class="notice notice-error is-dismissible">
-            <p><?php printf( __( '<strong>Notice:</strong> %s requires Easy Digital Downloads 2.5 or higher in order to function properly.', 'hsids-training' ), EDD_COMMISSIONS_PAYOUTS_PLUGIN_NAME ); ?></p>
-        </div>
-        
-        <?php
-    } );
-
-    return;
-}
-
-if ( version_compare( $wp_version, '4.2', '<' ) ) {
-    /**
-     * Display admin notice if not up to date with WP version
-     * 
-     * @codeCoverageIgnore
-     */
-    add_action( 'admin_notices', function() {
-        ?>
-        
-        <div class="notice notice-error is-dismissible">
-            <p><?php printf( __( '<strong>Notice:</strong> %s requires WordPress 4.2 or higher in order to function properly.', 'hsids-training' ), EDD_COMMISSIONS_PAYOUTS_PLUGIN_NAME ); ?></p>
-        </div>
-        
-        <?php
-    } );
-
-    return;
-}
 
 // Load Composer
-require EDD_COMMISSIONS_PAYOUTS_PLUGIN_DIR . 'vendor/autoload.php';
-require EDD_COMMISSIONS_PAYOUTS_PLUGIN_DIR . 'includes/class-hsids-training.php';
+require WP_GRUNT_STARTER_PLUGIN_PLUGIN_DIR . 'vendor/autoload.php';
+require WP_GRUNT_STARTER_PLUGIN_PLUGIN_DIR . 'includes/class-wp-grunt-starter-plugin.php';
 
-require EDD_COMMISSIONS_PAYOUTS_PLUGIN_DIR . 'includes/misc-functions.php';
 
-function EDD_Commissions_Payouts() {
-    return EDD_Commissions_Payouts::instance();
+function WP_Grunt_Starter_Plugin() {
+    return WP_Grunt_Starter_Plugin::instance();
 }
-EDD_Commissions_Payouts();
+WP_Grunt_Starter_Plugin();
